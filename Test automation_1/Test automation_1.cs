aeac12d6f7fca2ffc20cfbd21a00d4fe26cd6db8
dnaveenmcr <dnaveen.mcr@gmail.com>
2023-10-11 16:46:58 +0530

    change in script

--- Test automation_1/Test automation_1.cs
+++ Test automation_1/Test automation_1.cs
@@ -18,7 +18,7 @@ namespace Test_automation_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Hello World");
+			engine.GenerateInformation("Hello Dataminer");
 
 
         }
